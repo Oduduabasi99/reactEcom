ea94407b153ae0fe34037a0e2a9aa7e03bc06815
Oduduabasi99 <kellywizzyisong@gmail.com>
2022-04-22 22:09:27 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,113 @@
+import Slide1 from "./img/slider/slide-1.png";
+import Slide2 from "./img/slider/slide-2.png";
+import Slide3 from "./img/slider/slide-3.png";
+import Cat1 from "./img/categories/cat-1.png";
+import Cat2 from "./img/categories/cat-2.png";
+import Cat3 from "./img/categories/cat-3.png";
+import Cat4 from "./img/categories/cat-4.png";
+import Cat5 from "./img/categories/cat-5.png";
+import Pro1 from "./img/products/pro-1.png";
+import Pro2 from "./img/products/pro-2.png";
+import Pro3 from "./img/products/pro-3.png";
+import Pro4 from "./img/products/pro-4.png";
+import Pro5 from "./img/products/pro-5.png";
+import Pro6 from "./img/products/pro-6.png";
+import Pro7 from "./img/products/pro-7.png";
+import Pro8 from "./img/products/pro-8.png";
+import Pro9 from "./img/products/pro-9.png";
+import Pro10 from "./img/products/pro-10.png";
+
+export const sliderItems = [
+    {
+        id: 1,
+        img: Slide1,
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON HEALTH! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: Slide2,
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON HEALTH! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: Slide3,
+        title: "POPULAR SALE",
+        desc: "DON'T COMPROMISE ON HEALTH! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+    }
+];
+
+export const categories = [
+    {
+      id: 1,
+      img: Cat1,
+      title: "FRUIT",
+    },
+    {
+      id: 2,
+      img: Cat2,
+      title: "FRUIT",
+    },
+    {
+      id: 3,
+      img: Cat3,
+      title: "FRUIT",
+    },
+    {
+      id: 4,
+      img: Cat4,
+      title: "FRUIT",
+    },
+    {
+      id: 5,
+      img: Cat5,
+      title: "FRUIT",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img: Pro1,
+    },
+    {
+      id:2,
+      img: Pro2,
+    },
+    {
+      id:3,
+      img: Pro3,
+    },
+    {
+      id:4,
+      img: Pro4,
+    },
+      {
+          id: 5,
+          img: Pro5,
+      },
+    {
+      id:6,
+      img: Pro6,
+    },
+    {
+      id:7,
+      img: Pro7,
+    },
+    {
+      id:8,
+      img: Pro8,
+    },
+    {
+      id:9,
+      img: Pro9,
+    },
+    {
+      id:10,
+      img: Pro10,
+    },
+  ]
